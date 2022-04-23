f5a514d1fbf16d1d16e04404923ad3d0e83a9fcd
Tanvir Ahmed Sojal <tanvirahmedsojal@gmail.com>
2022-04-23 22:52:56 +0600

    added UCC products

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const puppeteer = require('puppeteer');
+const fs = require('fs');
 
 const URLs = {
     StarTech:
         'https://www.startech.com.bd/component/graphics-card?filter_status=7',
     TechLand: 'https://www.techlandbd.com/graphics-card?fq=1',
-    UCC: 'https://www.ucc-bd.com/pc-components/graphics-card-gpu.html',
+    UCC: 'https://www.ucc-bd.com/pc-components/graphics-card-gpu.html?product_list_limit=all',
 };
 
 class GpuDataProvider {
-    async getStarTechGPUs() {
+    async getGPUsFromStarTech() {
         const browser = await puppeteer.launch();
         const page = await browser.newPage();
         page.setViewport({ width: 1920, height: 1080 });
@@ -65,18 +66,75 @@ class GpuDataProvider {
         return gpus;
     }
 
-    async getTechLandGPUs() {
+    async getGPUsFromTechLand() {
         const browser = await puppeteer.launch();
         const page = await browser.newPage();
         page.setViewport({ width: 1920, height: 1080 });
 
-        await page.goto(URLs.TechLand);
+        let URL = URLs.TechLand;
 
-        const gpus = await page.evaluate(() => {
-            const elementNodeList = document.querySelectorAll('.product-thumb');
+        const gpus = [];
+
+        while (URL) {
+            await page.goto(URL);
+
+            const gpusFromPage = await page.evaluate(() => {
+                const elementNodeList =
+                    document.querySelectorAll('.product-thumb');
+                const elements = [...elementNodeList];
+                return elements.map((element) => {
+                    const name = element.querySelector('.name').innerText;
+
+                    let price = element.querySelector('.price-new');
+                    if (price == null) {
+                        price = element.querySelector('.price');
+                    }
+                    price = price.innerText.replace(/[^0-9.]/g, '');
+                    price = parseFloat(price);
+
+                    const shop = 'TechLand';
+
+                    const url = element
+                        .querySelector('.name')
+                        .getElementsByTagName('a')[0].href;
+
+                    return { name, price, shop, url };
+                });
+            });
+
+            gpus.push(...gpusFromPage);
+
+            const nextPageUrl = await page.evaluate(() => {
+                const pagination = document.querySelector('.pagination');
+                const nextButton = pagination.querySelector('.next');
+                return nextButton ? nextButton.href : false;
+            });
+
+            URL = nextPageUrl;
+        }
+
+        await browser.close();
+
+        return gpus;
+    }
+
+    async getGPUsFromUCC() {
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+        page.setViewport({ width: 1920, height: 1080 });
+
+        let URL = URLs.UCC;
+        const gpus = [];
+
+        await page.goto(URL);
+
+        const gpusFromPage = await page.evaluate(() => {
+            const elementNodeList =
+                document.querySelectorAll('.product-item-info');
             const elements = [...elementNodeList];
             return elements.map((element) => {
-                const name = element.querySelector('.name').innerText;
+                const name =
+                    element.querySelector('.product-item-link').innerText;
 
                 let price = element.querySelector('.price-new');
                 if (price == null) {
@@ -85,15 +143,15 @@ class GpuDataProvider {
                 price = price.innerText.replace(/[^0-9.]/g, '');
                 price = parseFloat(price);
 
-                const shop = 'TechLand';
+                const shop = 'UCC';
 
-                const url = element
-                    .querySelector('.name')
-                    .getElementsByTagName('a')[0].href;
+                const url = element.querySelector('.product-item-link').href;
 
                 return { name, price, shop, url };
             });
         });
+
+        gpus.push(...gpusFromPage);
 
         await browser.close();
 
@@ -107,12 +165,14 @@ class GpuDataProvider {
     async getGpuData() {
         const gpus = [];
 
-        //gpus.push(...(await this.getTechLandGPUs()));
+        gpus.push(...(await this.getGPUsFromStarTech()));
+        gpus.push(...(await this.getGPUsFromTechLand()));
+        gpus.push(...(await this.getGPUsFromUCC()));
+        gpus.sort((gpu) => gpu.price);
 
-        gpus.push(...(await this.getStarTechGPUs()));
-        //return gpus;
+        return gpus;
 
-        return gpus.map((gpu) => this.getPrintString(gpu));
+        //return gpus.map((gpu) => this.getPrintString(gpu));
     }
 }
 
@@ -122,5 +182,7 @@ class GpuDataProvider {
     // console.log(printGpus);
 
     const gpuDataProvider = new GpuDataProvider();
-    console.log(await (await gpuDataProvider.getGpuData()).length);
+    const gpus = await gpuDataProvider.getGpuData();
+
+    console.log(gpus.length);
 })();
